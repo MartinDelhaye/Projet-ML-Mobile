07aafd1dda7702e4e466f698bc64d6fe656315ca
MartinDelhaye <delhayemar1@gmail.com>
2026-04-23 16:31:59 -0400

    chore : add  dépendances pour la nav, ViewModel et Retrofit

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -40,7 +40,11 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
